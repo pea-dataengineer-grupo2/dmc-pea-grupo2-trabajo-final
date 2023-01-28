31f6f72e7dfd8d8e2ea25803d04d5da634a04d99
Manuel Delgado <manuel.delgado@grupokonecta.com>
2023-01-28 15:57:23 -0500

    fix(global): updating upload to workload layer file

---
+++ src/uploadtoworkloadlayer.py
@@ -0,0 +1,33 @@
+from google.cloud import storage
+
+
+def copy_blob(
+    bucket_name, blob_name, destination_bucket_name, destination_blob_name
+):
+    """Copies a blob from one bucket to another with a new name."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+    # destination_bucket_name = "destination-bucket-name"
+    # destination_blob_name = "destination-object-name"
+
+    storage_client = storage.Client()
+
+    source_bucket = storage_client.bucket(bucket_name)
+    source_blob = source_bucket.blob(blob_name)
+    destination_bucket = storage_client.bucket(destination_bucket_name)
+
+    blob_copy = source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_blob_name
+    )
+
+    print(
+        "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+            source_blob.name,
+            source_bucket.name,
+            blob_copy.name,
+            destination_bucket.name,
+        )
+    )
+
+copy_blob ("dmc-pea-de-grupo-2-input/MunicipalidadJesusMaria", "MuniJesusMaria2021_2.csv",
+ "dmc-pea-de-grupo-2-datalake/workload/MunicipalidadJesusMaria", "MuniJesusMaria2021_2.csv")
